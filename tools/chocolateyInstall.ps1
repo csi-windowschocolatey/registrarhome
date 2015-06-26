7736b0f24029642b25e194d7c086b8ef4e009200
DarwinJS <DarwinJS@sanoys.com>
2015-06-26 16:10:46 -0400

    first commit

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'applicationcompatibilitytoolkit'
+$installerType = 'EXE'
+$url = 'http://download.microsoft.com/download/D/3/3/D33A8F5D-A7E1-4BBE-B04D-7A688785A229/ApplicationCompatibilityToolkitSetup.exe'
+$silentArgs = '/C:"msiexec.exe /i act.msi /l*v $env:temp\CHOCO-INSTALL-applicationcompatibilitytoolkit.log /qn'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
---
+++ tools/chocolateyUninstall.ps1
@@ -0,0 +1,2 @@
+
+Uninstall-ChocolateyPackage 'applicationcompatibilitytoolkit' 'MSI' "/l*v $env:temp\CHOCO-UNINSTALL-applicationcompatibilitytoolkit.log /qn" 'act.msi'
